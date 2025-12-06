06229e1df2a977d89c490bed75413e3693182ca1
Daniel Lehoczki <lehoczki.dani5@gmail.com>
2025-12-06 18:49:20 +0100

    smtp error

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,18 +7,14 @@ const path = require("path")
 const host_url = process.env.host_url
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
-    user: process.env.EMAIL_USER, // your email
-    pass: process.env.EMAIL_PASS  // your app password
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
   }
 });
-
-transporter.verify((err, success) => {
-  if (err) console.log("SMTP Error:", err);
-  else console.log("SMTP ready");
-});
-
 
 async function registerUser(req, res) {
     try {
